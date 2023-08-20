36a810523e80cc5df5ae91d563596e387d4369b5
sejin park <seijin0722@naver.com>
2023-08-20 22:00:16 +0900

    📝 LV.1 코딩테스트 연습문제

--- LV_1/나누어떨어지는숫자배열/Soultion1.java
+++ LV_1/나누어떨어지는숫자배열/Soultion1.java
@@ -11,23 +11,22 @@ public class Soultion1 {
 
         List<Integer> tempList = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-           if (arr[i] % divisor == 0) {
-               tempList.add(arr[i]);
-           }
+            if (arr[i] % divisor == 0) {
+                tempList.add(arr[i]);  // arr[i]를 divisor로 나눠떨어지는것만 리스트로 저장
+            }
         }
         final int SIZE = tempList.size();
 
-        if (SIZE > 0) {
+        if (SIZE > 0) { // 리스트에 1개라도 있을때는 배열로 옮겨서 저장
             answer = new int[SIZE];
             for (int i = 0; i < SIZE; i++) {
                 answer[i] = tempList.get(i);
             }
             Arrays.sort(answer);
-        }else {
+        }else { // 리스트에 1개도 없을때는 -1 저장
             answer = new int[1];
             answer[0] = -1;
         }
-
         return answer;
     }
 
--- LV_1/서울에서김서방찾기/Solution1.java
+++ LV_1/서울에서김서방찾기/Solution1.java
@@ -1,2 +1,15 @@
-package LV_1.서울에서김서방찾기;public class Solution1 {
+package LV_1.서울에서김서방찾기;
+
+import java.util.Arrays;
+
+public class Solution1 {
+    public String solution(String[] seoul) {
+        return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") +"에 있다";
+    }
+
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+        Solution1 solution1 = new Solution1();
+        solution1.solution(seoul);
+    }
 }
