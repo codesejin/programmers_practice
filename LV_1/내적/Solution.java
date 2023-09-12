3ef6709f0483cf9e33be44c8afeabb8de53edcb9
sejin park <seijin0722@naver.com>
2023-09-12 18:15:17 +0900

    📝 LV_1 코딩테스트 연습문제

--- LV_1/내적/Solution.java
+++ LV_1/내적/Solution.java
@@ -1,2 +1,19 @@
-package LV_1.내적;public class Solution {
+package LV_1.내적;
+
+public class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        int len = a.length;
+        for (int i = 0; i < len ; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+        System.out.println(s.solution(a,b));
+    }
 }
