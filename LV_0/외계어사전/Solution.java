8ba74ed80a5e7e8076fc24c4d0a2f6b2a0897a16
sejin park <seijin0722@naver.com>
2023-10-21 22:40:51 +0900

    📝 LV_0 코딩테스트 연습문제

---
+++ LV_0/외계어사전/Solution.java
@@ -0,0 +1,29 @@
+package LV_0.외계어사전;
+
+
+public class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        for (String k : dic) {
+            boolean allCheck = true;
+            for (String x : spell) {
+                if (k.indexOf(x) == -1) {
+                    allCheck = false;
+                    break;
+                }
+            }
+            if (allCheck) answer = 1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        String[] spell1 = {"p", "o", "s"};
+        String[] spell2 = {"z", "d", "x"};
+        String[] dic1 = {"sod", "eocd", "qixm", "adio", "soo"};
+        String[] dic2 = {"def", "dww", "dzx", "loveaw"};
+        String[] dic3 ={"pppp", "oooo", "ssss", "spso"};
+        System.out.println(T.solution(spell2, dic2));
+    }
+}
