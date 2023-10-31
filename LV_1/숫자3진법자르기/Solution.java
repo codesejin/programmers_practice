603e06842cda3267a9859ab6bd829ec4f9a5d6be
sejin park <seijin0722@naver.com>
2023-10-31 10:40:19 +0900

    :memo: LV_1 코딩테스트 연습문제

--- LV_1/숫자3진법자르기/Solution.java
+++ LV_1/숫자3진법자르기/Solution.java
@@ -1,2 +1,14 @@
-package LV_1.숫자3진법자르기;public class Solution {
+package LV_1.숫자3진법자르기;
+
+public class Solution {
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3)).reverse();
+        return Integer.parseInt(sb.toString(),3);
+    }
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int n = 45;
+        System.out.println(T.solution(n));
+
+    }
 }
