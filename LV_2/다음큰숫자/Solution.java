9726fd4e1ac77baaf6b7dd02a37c52c075dc5e3c
sejin park <seijin0722@naver.com>
2023-11-12 19:35:21 +0900

    :memo: LV_2 코딩테스트 연습문제

--- LV_2/다음큰숫자/Solution.java
+++ LV_2/다음큰숫자/Solution.java
@@ -1,2 +1,29 @@
-package LV_2.다음큰숫자;public class Solution {
+package LV_2.다음큰숫자;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0, binaryNCnt = 0;
+        String binaryN = Integer.toString(n, 2);
+        for (int i = 0; i < binaryN.length(); i++) {
+            if (binaryN.charAt(i) == '1') binaryNCnt++;
+        }
+        for (int i = n+1; i < n+10; i++) {
+            String binaryI = Integer.toString(i, 2);
+            int binaryICnt = 0;
+            for (int j = 0; j < binaryI.length(); j++) {
+                if (binaryI.charAt(j) == '1') binaryICnt++;
+            }
+            if (binaryNCnt == binaryICnt) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int n = 78;
+        System.out.println(T.solution(n));
+    }
 }
--- LV_2/숫자의_표현/TwoPointers/Test.java
+++ LV_2/숫자의_표현/TwoPointers/Test.java
@@ -1,2 +1,22 @@
-package LV_2.숫자의_표현.TwoPointers;public class Test {
+package LV_2.숫자의_표현.TwoPointers;
+
+public class Test {
+    public int solution(int n) {
+        int answer = 0, lt = 1, sum = 0;
+        for (int rt = 1; rt <= n; rt++) {
+            sum += rt;
+            if (sum == n) answer ++;
+            while(sum >= n) {
+                sum -= lt;
+                lt++;
+                if (sum == n) answer ++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Test T = new Test();
+        int n = 15;
+        System.out.println(T.solution(n));
+    }
 }
