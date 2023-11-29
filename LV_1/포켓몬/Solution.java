a6edc174a7e194246606a097c5c98b6dce603b18
sejin park <seijin0722@naver.com>
2023-11-30 01:27:38 +0900

    :memo: LV_1 코딩테스트 연습문제

--- LV_1/포켓몬/Solution.java
+++ LV_1/포켓몬/Solution.java
@@ -1,23 +1,23 @@
 package LV_1.포켓몬;
 
 import java.util.HashMap;
+import java.util.Map;
 
 // 해쉬맵
 public class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i ++) {
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        int maxTake = nums.length / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x,0)+1);
         }
-        int max = nums.length/2;
-        if (max >= map.size()) answer = map.size();
-        else answer = max;
-        return  answer;
+        if (maxTake >= map.size()) return map.size();
+        else return maxTake;
     }
+
     public static void main(String[] args) {
         Solution T = new Solution();
-        int[] nums = {3,1,2,3};
+        int[] nums = {3, 1, 2, 3};
         System.out.println(T.solution(nums));
     }
 }
