56dbdd783918733be168a435ebfeeb1ee2fc26f9
sejin park <seijin0722@naver.com>
2024-03-24 09:42:10 +0900

    :memo: LV_Silver

--- LV_2/행렬테두리회전하기/Solution.java
+++ LV_2/행렬테두리회전하기/Solution.java
@@ -2,6 +2,11 @@ package LV_2.행렬테두리회전하기;
 
 import java.util.Arrays;
 
+/**
+ * 행렬 알고리즘
+ * @See https://velog.io/@ckr3453/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%96%89%EB%A0%AC-%ED%85%8C%EB%91%90%EB%A6%AC-%ED%9A%8C%EC%A0%84%ED%95%98%EA%B8%B0-Java
+ * @See https://rovictory.tistory.com/17
+ */
 public class Solution {
 
     public static void main(String[] args) {
@@ -19,11 +24,14 @@ public class Solution {
         int minimalsIdx=0;
 
         for(int[] query : queries){
+            // 인덱스
             int x1 = query[0]-1;
             int y1 = query[1]-1;
             int x2 = query[2]-1;
             int y2 = query[3]-1;
+            // 시작할 넘버
             int firstNum = square[x1][y2];
+            // 행렬을 돌리면서 최솟값을 찾아서 리턴해야함
             int min = firstNum;
 
             // 숫자를 우로 이동 (상단)
