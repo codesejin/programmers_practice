feef2f833a19e9f27cac751a8c4c3e64ac638277
sejin park <seijin0722@naver.com>
2024-04-11 19:34:10 +0900

    :memo: LV_1

--- LV_1/모의고사/Solution.java
+++ LV_1/모의고사/Solution.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class Solution {
     public int[] solution(int[] answers) {
+
         int[] first = {1,2,3,4,5}; // 5개씩 반복
         int[] second = {2,1,2,3,2,4,2,5}; // 8개씩 반복
         int[] third = {3,3,1,1,2,2,4,4,5,5}; // 10개씩 반복
-        int answer1=0, answer2 =0, answer3 =0;
+        int answer1 = 0, answer2 = 0, answer3 =0;
 
         // 수포자들의 점수 계산
         for(int i=0; i<answers.length; i++) {
             if(answers[i] == first[i%5]) answer1++;
             if(answers[i] == second[i%8]) answer2++;
             if(answers[i] == third[i%10]) answer3++;
-
         }
 
         // 최대 점수 구하기
